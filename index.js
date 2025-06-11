1b577b4f27d7d9bcaebba14045fe384592380c18
hamadolly <75244998+hamdolly@users.noreply.github.com>
2025-06-11 21:36:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import cors from 'cors'
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://receipt-counter.w3spaces.com",
+}));
 
 app.get('/', async (req, res) => {
     res.send("Hello world war Z!");
